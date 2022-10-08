78839c487949a7d3a1023e5aaa44ac4ec798662f
Angelique Rivera <angeliqueriveramorales@gmail.com>
2022-10-08 17:11:01 -0400

    feat: update SquareTarget component and CSS styling

--- src/components/practice-grids/SquareTarget.jsx
+++ src/components/practice-grids/SquareTarget.jsx
@@ -4,17 +4,14 @@ export default function SquareTarget() {
       <header className="container">
         <h2>Square Target Practice</h2>
       </header>
-      <section className="container target">
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
+      <section className="container">
+        <div className="container target">
+          <div className="target-left-wall"></div>
+          <div className="target-ceiling"></div>
+          <div className="target-right-wall"></div>
+          <div className="target-floor"></div>
+          <div className="bullseye"></div>
+        </div>
       </section>
     </article>
   );
