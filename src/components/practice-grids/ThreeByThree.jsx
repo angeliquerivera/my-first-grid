307c46018d3375dc09a85c1e2b405d28f22dd41a
Angelique Rivera <angeliqueriveramorales@gmail.com>
2022-10-08 17:53:06 -0400

    fix: center ThreeByThree component

--- src/components/practice-grids/ThreeByThree.jsx
+++ src/components/practice-grids/ThreeByThree.jsx
@@ -6,7 +6,7 @@ export default function ThreeByThree() {
       </header>
 
       <section className="container">
-        <div className="three-by-three">
+        <div className="container three-by-three">
           <div>1</div>
           <div>2</div>
           <div>3</div>
